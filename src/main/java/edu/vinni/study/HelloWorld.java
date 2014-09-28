0b879cada8f1b785e6e52e28b388ea591fc22124
f1llon <fav1990@gmail.com>
2014-09-28 21:12:35 +0100

    created HelloWorld application

---
+++ src/main/java/edu/vinni/study/HelloWorld.java
@@ -0,0 +1,13 @@
+package edu.vinni.study;
+
+/**
+ * @author anton.v.filimonov@gmail.com
+ * @since 28.09.2014
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+
+}
