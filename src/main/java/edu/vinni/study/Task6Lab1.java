b7c53296380efb4955fb9b5da3851e3de61cccf9
vynychuck <vynychuck@gmail.com>
2014-09-30 18:03:02 +0300

    Task 1.6 (lab 1, task 6)

---
+++ src/main/java/edu/vinni/study/Task6Lab1.java
@@ -0,0 +1,22 @@
+package edu.vinni.study;
+
+/**
+ * Created by serghii on 30.09.14.
+ */
+public class Task6Lab1 {
+    public static void main(String[] args) {
+        double y = 0.0;
+        int a = 3;
+        double x = 2;
+        if(x > 10) {
+            y = 1 / x;
+        }
+        if (x <= 10 && x >= - 10 ) {
+            y = a * (x * x);
+        }
+        if (x < - 10){
+            y = Math.sin(x);
+        }
+        System.out.println("Значення y дорівнює " + y);
+    }
+}
