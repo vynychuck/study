f6ea3343600a65a1072cd35a082f03ded20d9110
f1llon <fav1990@gmail.com>
2014-09-30 22:00:05 +0100

    deleted lab1 class

--- src/main/java/edu/vinni/study/Task5Lab1.java
+++ src/main/java/edu/vinni/study/Task5Lab1.java
@@ -6,13 +6,10 @@ package edu.vinni.study;
 public class Task5Lab1 {
     public static void main(String args[]) {
         double x = Double.parseDouble(args[0]), y = Double.parseDouble(args[1]);
-        int a = 0,b = 0, r = Integer.parseInt(args[2]);
-        double radius, temp;
-        if (x != 0 && y != 0) {
-            temp = (x * x) + (y * y);
-        temp = r * Math.sqrt(2) / 2;
-            radius = r * Math.sqrt(2);
-        System.out.println("Точка з кординатами х, у є точкою перетину діагоналей квадрата зі стороною r, якщо наступне рівняння справджується " + (temp = radius / 2));
+        int length = Integer.parseInt(args[2]);
+        final double radius = length * Math.sqrt(2) / 2;
+        if (x * x + y * y == radius * radius) {
+            System.out.println("TRUE");
         }
     }
 }
