cdff0c78a86e952175c76c1d570f462f07b53d27
vynychuck <vynychuck@gmail.com>
2014-10-01 22:23:41 +0300

    приклад номер 10 з першої лаби

---
+++ src/main/java/edu/vinni/study/Task1Lab2.java
@@ -0,0 +1,14 @@
+package edu.vinni.study;
+
+/**
+ * Created by serghii on 01.10.14.
+ */
+public class Task1Lab2 {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            throw new IllegalArgumentException("More or less input sources than 1");
+        }
+        double a = Double.parseDouble(args[0]);
+        System.out.println(Math.signum(a));
+    }
+}
