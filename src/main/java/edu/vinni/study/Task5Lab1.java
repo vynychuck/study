7266a99003005c55757bb5bf0eed35d69a733291
vynychuck <vynychuck@gmail.com>
2014-10-01 14:53:24 +0300

    added exception

--- src/main/java/edu/vinni/study/Task5Lab1.java
+++ src/main/java/edu/vinni/study/Task5Lab1.java
@@ -5,6 +5,9 @@ package edu.vinni.study;
  */
 public class Task5Lab1 {
     public static void main(String args[]) {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Input sources more than 3. ");
+        }
         double x = Double.parseDouble(args[0]), y = Double.parseDouble(args[1]);
         int length = Integer.parseInt(args[2]);
         final double radius = length * Math.sqrt(2) / 2;
