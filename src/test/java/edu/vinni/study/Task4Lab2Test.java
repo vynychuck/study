83c4cf62e8742fc642e2ac603caad8b5b10498d1
vynychuck <vynychuck@gmail.com>
2014-10-04 00:37:32 +0300

    unit test for task 4 lab 2 renewed

--- src/test/java/edu/vinni/study/Task4Lab2Test.java
+++ src/test/java/edu/vinni/study/Task4Lab2Test.java
@@ -3,20 +3,17 @@ package edu.vinni.study;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static edu.vinni.study.Task4Lab2.*;
+
 /**
  * Created by serghii on 03.10.14.
  */
 public class Task4Lab2Test {
 
-
-
     @Test
     public void testMainRightPath() {
-        Assert.assertEquals(2, Math.abs(2));
-        Assert.assertEquals(2, Math.abs(-2));
+        Assert.assertEquals(2, abs(2));
+        Assert.assertEquals(2, abs(-2));
     }
-
-
-
 }
 
