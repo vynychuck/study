37440711da793593b543606dd7f517ee34adbf7e
vynychuck <vynychuck@gmail.com>
2014-10-05 06:23:06 +0300

    included step of progression in method parameters

--- src/main/java/edu/vinni/study/Task1Lab3.java
+++ src/main/java/edu/vinni/study/Task1Lab3.java
@@ -4,8 +4,7 @@ package edu.vinni.study;
  * Created by serghii on 05.10.14.
  */
 public class Task1Lab3 {
-    public static int showAriphProgr(int a) {
-        int n = 0;
+    public static int showAriphProgr(int a, int n) {
         while (a < 100) {
             a += n;
             System.out.println(a);
