f95430e6dd58c1ac288dc68edc4df02203422354
vynychuck <vynychuck@gmail.com>
2014-10-06 15:58:57 +0300

    re-tested

--- src/test/java/edu/vinni/study/Task4Lab3Test.java
+++ src/test/java/edu/vinni/study/Task4Lab3Test.java
@@ -16,15 +16,15 @@ public class Task4Lab3Test {
     public void testMainPath() {
         TreeSet<Integer> hs = new TreeSet<Integer>();
         hs.add(21);
-        hs.add(5);
+        hs.add(3);
         hs.add(7);
         hs.add(9);
         hs.add(11);
         hs.add(13);
+        hs.add(19);
         hs.add(15);
         hs.add(17);
-        hs.add(19);
-        hs.add(3);
+        hs.add(5);
         Assert.assertEquals(hs, Task4Lab3.showAriphNumb(3, 2));
     }
 }
