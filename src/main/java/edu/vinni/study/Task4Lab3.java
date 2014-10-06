5ec792d2b9a82c50132b8c961bd61a32161ef729
vynychuck <vynychuck@gmail.com>
2014-10-06 03:32:51 +0300

    test of commiting and pushing

---
+++ src/main/java/edu/vinni/study/Task4Lab3.java
@@ -0,0 +1,11 @@
+package edu.vinni.study;
+
+/**
+ * Created by serghii on 06.10.14.
+ */
+public class Task4Lab3 {
+    public static void main(String[] args) {
+        int a = 1;
+        System.out.println(a);
+    }
+}
