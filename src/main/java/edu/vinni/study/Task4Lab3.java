91839304c4420c0d7043f72144a2849bad8b0f46
vynychuck <vynychuck@gmail.com>
2014-10-06 05:58:59 +0300

    task 4 lab 3 and unit test for it

--- src/main/java/edu/vinni/study/Task4Lab3.java
+++ src/main/java/edu/vinni/study/Task4Lab3.java
@@ -4,8 +4,13 @@ package edu.vinni.study;
  * Created by serghii on 06.10.14.
  */
 public class Task4Lab3 {
-    public static void main(String[] args) {
-        int a = 1;
-        System.out.println(a);
+    public static int showAriphNumb(int a, int b) {
+        int x = 0;
+        while (x < 10) {
+            System.out.println(a);
+            a += b;
+            x++;
+        }
+        return 0;
     }
 }
---
+++ src/test/java/edu/vinni/study/Task4Lab3Test.java
@@ -0,0 +1,15 @@
+package edu.vinni.study;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by serghii on 06.10.14.
+ */
+public class Task4Lab3Test {
+
+    @Test
+    public void testMainPath() {
+        Assert.assertEquals(0, Task4Lab3.showAriphNumb(3, 2));
+    }
+}
