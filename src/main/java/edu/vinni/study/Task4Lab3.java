54e010b2bca2c29224891105fb88925b2350d5d1
vynychuck <vynychuck@gmail.com>
2014-10-06 15:19:51 +0300

    rewritten function with returning set of first 10 elements

--- src/main/java/edu/vinni/study/Task4Lab3.java
+++ src/main/java/edu/vinni/study/Task4Lab3.java
@@ -1,16 +1,20 @@
 package edu.vinni.study;
 
+import java.util.HashSet;
+import java.util.TreeSet;
+
 /**
  * Created by serghii on 06.10.14.
  */
 public class Task4Lab3 {
-    public static int showAriphNumb(int a, int b) {
+    public static TreeSet<Integer> showAriphNumb(int a, int b) {
         int x = 0;
+        TreeSet<Integer> hs = new TreeSet<Integer>();
         while (x < 10) {
-            System.out.println(a);
+            hs.add(a);
             a += b;
             x++;
         }
-        return 0;
+        return hs;
     }
 }
--- src/test/java/edu/vinni/study/Task4Lab3Test.java
+++ src/test/java/edu/vinni/study/Task4Lab3Test.java
@@ -1,7 +1,11 @@
 package edu.vinni.study;
 
+import apple.laf.JRSUIUtils;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.TreeSet;
 
 /**
  * Created by serghii on 06.10.14.
@@ -10,6 +14,17 @@ public class Task4Lab3Test {
 
     @Test
     public void testMainPath() {
-        Assert.assertEquals(0, Task4Lab3.showAriphNumb(3, 2));
+        TreeSet<Integer> hs = new TreeSet<Integer>();
+        hs.add(21);
+        hs.add(5);
+        hs.add(7);
+        hs.add(9);
+        hs.add(11);
+        hs.add(13);
+        hs.add(15);
+        hs.add(17);
+        hs.add(19);
+        hs.add(3);
+        Assert.assertEquals(hs, Task4Lab3.showAriphNumb(3, 2));
     }
 }
