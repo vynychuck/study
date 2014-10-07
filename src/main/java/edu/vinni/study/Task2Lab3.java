3b5c17cb05e4e2238fb1635da9d983a4f0b49345
vynychuck <vynychuck@gmail.com>
2014-10-07 11:50:37 +0300

    TreeSet changed to ArrayList

--- src/main/java/edu/vinni/study/Task2Lab3.java
+++ src/main/java/edu/vinni/study/Task2Lab3.java
@@ -1,14 +1,15 @@
 package edu.vinni.study;
 
-import java.util.TreeSet;
+import java.util.ArrayList;
+
 
 /**
  * Created by serghii on 06.10.14.
  */
 public class Task2Lab3 {
-    public static TreeSet<Integer> checkNumb(int a) {
+    public static ArrayList<Integer> checkNumb(int a) {
         int b = 0;
-        TreeSet<Integer> ts = new TreeSet<Integer>();
+        ArrayList<Integer> ts = new ArrayList<Integer>();
         while (b <= 300) {
             b++;
             if (b % a == 0) {
--- src/test/java/edu/vinni/study/Task2Lab3Test.java
+++ src/test/java/edu/vinni/study/Task2Lab3Test.java
@@ -3,6 +3,7 @@ package edu.vinni.study;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.TreeSet;
 
 /**
@@ -11,7 +12,7 @@ import java.util.TreeSet;
 public class Task2Lab3Test {
     @Test
     public void testMainRightPath(){
-        TreeSet<Integer> ts = new TreeSet<Integer>();
+        ArrayList<Integer> ts = new ArrayList<Integer>();
         for (int i = 4; i <= 300; i += 4 ) {
             ts.add(i);
         }
