84b950e4166dda4f4dd7602976b0cace2d92067c
Anton Filimonov <anton.v.filimonov@gmail.com>
2014-10-08 22:16:11 +0100

    Changed ArrayList to List

--- src/main/java/edu/vinni/study/Task6Lab3.java
+++ src/main/java/edu/vinni/study/Task6Lab3.java
@@ -1,20 +1,22 @@
 package edu.vinni.study;
 
 import java.util.ArrayList;
+import java.util.List;
+
 
 
 /**
  * Created by serghii on 06.10.14.
  */
 public class Task6Lab3 {
-    public static ArrayList<Integer> showGeoProgr(int a, int b) {
-        ArrayList<Integer> ts = new ArrayList<Integer>();
+    public static List<Integer> showGeoProgr(int a, int b) {
+        List<Integer> list = new ArrayList<Integer>();
         int x = 0;
         while (x < 10){
-            ts.add(a);
+            list.add(a);
             a *= b;
             x++;
         }
-        return ts;
+        return list;
     }
 }
