568c09a228e4331d6b4bad01c0c982c7c6023ff1
vynychuck <vynychuck@gmail.com>
2014-10-09 14:17:19 +0300

    task 9 lab 3 and test for it (yak znaiti factorial chisla n)

--- src/main/java/edu/vinni/study/Task9Lab3.java
+++ src/main/java/edu/vinni/study/Task9Lab3.java
@@ -4,7 +4,12 @@ package edu.vinni.study;
  * Created by serghii on 09.10.14.
  */
 public class Task9Lab3 {
-    public static void main(String[] args) {
-
+    public static int getFact(int n) {
+        int fact = 1, i = 1;
+        while (i <= n) {
+            fact *= i;
+            i++;
+        }
+        return fact;
     }
 }
