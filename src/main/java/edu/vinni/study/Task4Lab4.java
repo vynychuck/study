93a3b8c99b54748b9ca35ee9e7f12e0e9015744a
vynychuck <vynychuck@gmail.com>
2014-10-10 14:05:59 +0300

    task 4 lab 4

---
+++ src/main/java/edu/vinni/study/Task4Lab4.java
@@ -0,0 +1,18 @@
+package edu.vinni.study;
+
+/**
+ * Created by serghii on 10.10.14.
+ */
+public class Task4Lab4 {
+    public static int getTenMembers(int startpoint, int step) {
+             int a = startpoint;
+            for (int i = 0; i < 10; i ++) {
+                System.out.println(a);
+                a +=step;
+
+            }
+
+
+        return 0;
+    }
+}
