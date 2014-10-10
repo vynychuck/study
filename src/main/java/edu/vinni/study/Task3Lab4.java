cc89b3e4a0ccd3f8de0d17499665e8d30b793512
vynychuck <vynychuck@gmail.com>
2014-10-10 12:52:04 +0300

    task 3 lab 4 and test

---
+++ src/main/java/edu/vinni/study/Task3Lab4.java
@@ -0,0 +1,14 @@
+package edu.vinni.study;
+
+/**
+ * Created by serghii on 10.10.14.
+ */
+public class Task3Lab4 {
+    public static int showAriphProgr(int a, int b) {
+        for (int i = a; i < 100; i += b) {
+            System.out.println(i);
+        }
+
+        return 0;
+    }
+}
---
+++ src/test/java/edu/vinni/study/Task3Lab4Test.java
@@ -0,0 +1,14 @@
+package edu.vinni.study;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by serghii on 10.10.14.
+ */
+public class Task3Lab4Test {
+    @Test
+    public void testMainPath(){
+        Assert.assertEquals(0, Task3Lab4.showAriphProgr(3, 2));
+    }
+}
