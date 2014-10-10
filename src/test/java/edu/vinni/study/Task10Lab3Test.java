d3bd5e54ecff5d11dca932f01e5de0df9beea644
vynychuck <vynychuck@gmail.com>
2014-10-10 08:37:33 +0300

    changed delta to 0

--- src/test/java/edu/vinni/study/Task10Lab3Test.java
+++ src/test/java/edu/vinni/study/Task10Lab3Test.java
@@ -10,6 +10,6 @@ public class Task10Lab3Test {
     @Test
     public void testMainPath(){
         Integer[] list = new Integer[]{185, 175, 180};
-        Assert.assertEquals(180, Task10Lab3.getAverageHeight(list), 5);
+        Assert.assertEquals(180, Task10Lab3.getAverageHeight(list), 0);
     }
 }
