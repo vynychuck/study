85af77ebdfd8bcd34da8b1aae0a4e106ff72619e
vynychuck <vynychuck@gmail.com>
2014-10-15 13:32:53 +0300

    few more exercises with tests

--- src/main/java/edu/vinni/study/Exercise.java
+++ src/main/java/edu/vinni/study/Exercise.java
@@ -1,13 +1,74 @@
 package edu.vinni.study;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by serghii on 15.10.14.
  */
 public class Exercise {
-    public static String CheckPassFail(int mark){
+    public static String CheckPassFail(int mark) {
         String positive = "PASS", negative = "FAIL";
-        if (mark >= 50){
+        if (mark >= 50) {
             return positive;
         } else return negative;
     }
+
+    public static String CheckOddEven(int mark) {
+        String odd = "Odd Number", even = "Even Number";
+        if (mark % 2 == 0) {
+            return even;
+        } else return odd;
+    }
+
+    public static String PrintNumberInWord(int mark) {
+        String one = "ONE", two = "TWO", three = "THREE", other = "OTHER";
+        switch (mark) {
+            case 1:
+                return one;
+            case 2:
+                return two;
+            case 3:
+                return three;
+            default:
+                return other;
+        }
+    }
+
+    public static String PrintDayInWord(int mark) {
+        String mon = "Monday", tue = "Tuesday", wed = "Wednesday", thur = "Thursday", fri = "Friday", sat = "Saturday",
+                sun = "Sunday", not = "Not a valid day";
+        switch (mark) {
+            case 1:
+                return mon;
+            case 2:
+                return tue;
+            case 3:
+                return wed;
+            case 4:
+                return thur;
+            case 5:
+                return fri;
+            case 6:
+                return sat;
+            case 7:
+                return sun;
+            default:
+                return not;
+        }
+    }
+
+    public static List<Double> SumAndAverage(int lowerbound, int upperbound) {
+        List<Double> list = new ArrayList<Double>();
+        double sum = 0;
+        double average;
+        for (int i = lowerbound; i <= upperbound; ++i) {
+            sum += i;
+        }
+        list.add(sum);
+        average = sum / upperbound;
+        list.add(average);
+        return list;
+    }
+
 }
--- src/test/java/edu/vinni/study/ExerciseTest.java
+++ src/test/java/edu/vinni/study/ExerciseTest.java
@@ -3,13 +3,44 @@ package edu.vinni.study;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 /**
  * Created by serghii on 15.10.14.
  */
 public class ExerciseTest {
     @Test
-    public void testCheckPassFail(){
+    public void testCheckPassFail() {
         Assert.assertEquals("FAIL", Exercise.CheckPassFail(49));
         Assert.assertEquals("PASS", Exercise.CheckPassFail(135));
     }
+
+    @Test
+    public void testOddEven() {
+        Assert.assertEquals("Even Number", Exercise.CheckOddEven(18));
+        Assert.assertEquals("Odd Number", Exercise.CheckOddEven(13));
+    }
+
+    @Test
+    public void testPrintNumberInWord() {
+        Assert.assertEquals("THREE", Exercise.PrintNumberInWord(3));
+        Assert.assertEquals("OTHER", Exercise.PrintNumberInWord(235));
+    }
+
+    @Test
+    public void testPrintDayInWord() {
+        Assert.assertEquals("Monday", Exercise.PrintDayInWord(1));
+        Assert.assertEquals("Thursday", Exercise.PrintDayInWord(4));
+        Assert.assertEquals("Not a valid day", Exercise.PrintDayInWord(13));
+    }
+
+    @Test
+    public void testSumAndAverage() {
+        ArrayList<Double> list = new ArrayList<Double>() {
+        };
+        list.add(5050.0);
+        list.add(50.5);
+        Assert.assertEquals(list, Exercise.SumAndAverage(1, 100));
+    }
 }
+
