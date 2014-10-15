526a3b62f9cde2cc9661228139c7eadb01c7af0d
vynychuck <vynychuck@gmail.com>
2014-10-15 12:11:51 +0300

    now function is void

--- src/main/java/edu/vinni/study/Task1Lab4.java
+++ src/main/java/edu/vinni/study/Task1Lab4.java
@@ -4,10 +4,9 @@ package edu.vinni.study;
  * Created by serghii on 10.10.14.
  */
 public class Task1Lab4 {
-    public static int showPositive(int a){
+    public static void showPositive(int a){
         for (int i=1; i <= a; i++){
             System.out.println(i);
         }
-        return 0;
     }
 }
